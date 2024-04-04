f9b5aa718ad575d1c5fce03bc5a607a23f15641f
hyunji1203 <hj27720@gmail.com>
2024-04-05 01:37:07 +0900

    feat: 패키지 변경

--- app/src/main/java/com/example/seoulgonggong/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.seoulgonggong
+package com.example.seoulgonggong.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seoulgonggong.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/seoulgonggong/ui/main/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.seoulgonggong.ui.main
+
+class MainViewModel {
+}
