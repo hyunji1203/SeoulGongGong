8179c7355a0fe0bf2e159f26c1cbbd3bcfe7fe99
hyunji1203 <hj27720@gmail.com>
2024-04-07 23:28:02 +0900

    feat: 시간과 위치를 넣어서 데이터를 불러올 수 있도록 로직 변경

--- app/src/main/java/com/example/seoulgonggong/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/main/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.seoulgonggong.domain.model.BaseDateTime
 import com.example.seoulgonggong.domain.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -21,18 +22,20 @@ class MainViewModel
         val temp: LiveData<String> = _temp
 
         fun fetchWeather() {
+            val baseDateTime = BaseDateTime.getBaseDateTime()
+            val point = GeoPointConverter().convert(37.5532, 127.1906)
             viewModelScope.launch {
-                kotlin.runCatching {
+                runCatching {
                     weatherRepository.getWeather(
                         serviceKey = "N4E7Om1Q+ZfsXn5KbolmQMkKGMtW6NKltMv/tBhClqRm/U6UMiSeIamXBI9aD6GNs/GQRR6Maxah3UfAcw16+Q==",
-                        baseDate = "20240407",
-                        baseTime = "0500",
-                        nx = 55,
-                        ny = 127,
+                        baseDate = baseDateTime.baseDate,
+                        baseTime = baseDateTime.baseTime,
+                        nx = point.nx,
+                        ny = point.ny,
                     )
                 }.onSuccess { forecasts ->
-                    Log.d("test", forecasts[0].category)
-                    _temp.value = forecasts[0].category
+                    Log.d("test", forecasts[0].baseTime)
+                    _temp.value = forecasts[0].category.name
                 }.onFailure {
                     Log.d("test", "${it.message}")
                     throw NetworkErrorException("네트워크 오류에용")
