91747fcf5890f51cd30c251df7f0433806eba0d3
hyunji1203 <hj27720@gmail.com>
2024-04-07 18:23:43 +0900

    feat: 메인 뷰모델 생성 및 날씨 불러오는 함수 구현

--- app/src/main/java/com/example/seoulgonggong/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/main/MainViewModel.kt
@@ -1,5 +1,42 @@
 package com.example.seoulgonggong.ui.main
 
+import android.accounts.NetworkErrorException
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.seoulgonggong.domain.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel : ViewModel()
+@HiltViewModel
+class MainViewModel
+    @Inject
+    constructor(
+        private val weatherRepository: WeatherRepository,
+    ) : ViewModel() {
+        private val _temp = MutableLiveData<String>()
+        val temp: LiveData<String> = _temp
+
+        fun fetchWeather() {
+            viewModelScope.launch {
+                kotlin.runCatching {
+                    weatherRepository.getWeather(
+                        serviceKey = "N4E7Om1Q+ZfsXn5KbolmQMkKGMtW6NKltMv/tBhClqRm/U6UMiSeIamXBI9aD6GNs/GQRR6Maxah3UfAcw16+Q==",
+                        baseDate = "20240407",
+                        baseTime = "0500",
+                        nx = 55,
+                        ny = 127,
+                    )
+                }.onSuccess { forecasts ->
+                    Log.d("test", forecasts[0].category)
+                    _temp.value = forecasts[0].category
+                }.onFailure {
+                    Log.d("test", "${it.message}")
+                    throw NetworkErrorException("네트워크 오류에용")
+                }
+            }
+        }
+    }
