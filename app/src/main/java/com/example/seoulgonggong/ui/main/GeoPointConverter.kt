83ac375c8f968b3b10df635dc20f0f1e7abb58c8
hyunji1203 <hj27720@gmail.com>
2024-04-07 23:26:56 +0900

    feat: 위도 경도를 기상청 X, Y 좌표로 변환해주는 Converter 생성

---
+++ app/src/main/java/com/example/seoulgonggong/ui/main/GeoPointConverter.kt
@@ -0,0 +1,44 @@
+package com.example.seoulgonggong.ui.main
+
+import com.example.seoulgonggong.domain.model.Point
+
+class GeoPointConverter {
+    fun convert(
+        lon: Double,
+        lat: Double,
+    ): Point {
+        val RE = 6371.00877 // 지구 반경(km)
+        val GRID = 5.0 // 격자 간격(km)
+        val SLAT1 = 30.0 // 투영 위도1(degree)
+        val SLAT2 = 60.0 // 투영 위도2(degree)
+        val OLON = 126.0 // 기준점 경도(degree)
+        val OLAT = 38.0 // 기준점 위도(degree)
+        val XO = 43 // 기준점 X좌표(GRID)
+        val YO = 136 // 기준점 Y좌표(GRID)
+        val DEGRAD = Math.PI / 180.0
+        val re = RE / GRID
+        val slat1 = SLAT1 * DEGRAD
+        val slat2 = SLAT2 * DEGRAD
+        val olon = OLON * DEGRAD
+        val olat = OLAT * DEGRAD
+
+        var sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn)
+        var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sf = Math.pow(sf, sn) * Math.cos(slat1) / sn
+        var ro = Math.tan(Math.PI * 0.25 + olat * 0.5)
+        ro = re * sf / Math.pow(ro, sn)
+
+        var ra = Math.tan(Math.PI * 0.25 + (lon) * DEGRAD * 0.5)
+        ra = re * sf / Math.pow(ra, sn)
+        var theta = lat * DEGRAD - olon
+        if (theta > Math.PI) theta -= 2.0 * Math.PI
+        if (theta < -Math.PI) theta += 2.0 * Math.PI
+        theta *= sn
+
+        val x = (ra * Math.sin(theta) + XO + 0.5).toInt()
+        val y = (ro - ra * Math.cos(theta) + YO + 0.5).toInt()
+
+        return Point(x, y)
+    }
+}
