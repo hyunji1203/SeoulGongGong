ba3378f8cb7a79535a7b1b5858171a17c1401730
hyunji1203 <hj27720@gmail.com>
2024-04-07 18:22:43 +0900

    feat: 날씨 레포지토리 주입해주는 기능 구현

--- app/src/main/java/com/example/seoulgonggong/di/RepositoryModule.kt
+++ app/src/main/java/com/example/seoulgonggong/di/RepositoryModule.kt
@@ -1,3 +1,18 @@
 package com.example.seoulgonggong.di
 
-class RepositoryModule
+import com.example.seoulgonggong.data.repository.DefaultWeatherRepository
+import com.example.seoulgonggong.data.service.WeatherService
+import com.example.seoulgonggong.domain.repository.WeatherRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+    @Singleton
+    @Provides
+    fun provideWeatherRepository(weatherService: WeatherService): WeatherRepository = DefaultWeatherRepository(weatherService)
+}
