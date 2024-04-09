1b44accb7afe4c39ece6880f26a6a77bcf7e0f39
hyunji1203 <hj27720@gmail.com>
2024-04-09 18:02:40 +0900

    feat: 위치권한 함수 리팩토링 및 주소 불러오는 함수 임시 구현

--- app/src/main/java/com/example/seoulgonggong/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/main/MainActivity.kt
@@ -2,21 +2,22 @@ package com.example.seoulgonggong.ui.main
 
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat.checkSelfPermission
 import com.example.seoulgonggong.R
 import com.example.seoulgonggong.databinding.ActivityMainBinding
+import com.example.seoulgonggong.util.openSetting
+import com.example.seoulgonggong.util.showToast
 import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,29 +25,12 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     private val locationPermissionRequest =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions(),
-        ) { permissions ->
-            when {
-                permissions.getOrDefault(ACCESS_FINE_LOCATION, false) -> {
-                    checkLocationPermission()
-                }
-
-                permissions.getOrDefault(ACCESS_COARSE_LOCATION, false) -> {
-                    checkLocationPermission()
-                }
-
-                else -> {
-                    Toast.makeText(
-                        this,
-                        getString(R.string.main_location_access),
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    val intent =
-                        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                            data = Uri.fromParts("package", packageName, null)
-                        }
-                    startActivity(intent)
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val isGranted = it.value
+                if (isGranted.not()) {
+                    showToast(getString(R.string.main_location_access))
+                    openSetting()
                 }
             }
         }
@@ -80,20 +64,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkLocationPermission() {
         val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        if (checkSelfPermission(
-                this,
-                ACCESS_FINE_LOCATION,
-            ) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(
-                this,
-                ACCESS_COARSE_LOCATION,
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        if (checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             locationPermissionRequest.launch(locationPermissions)
             return
         }
         fusedLocationClient.lastLocation.addOnSuccessListener {
             Log.d("test", "${it.latitude}, ${it.longitude}")
             viewModel.fetchWeather(it.latitude, it.longitude)
+//            setTown(it.latitude, it.longitude)
+        }
+    }
+
+    // 수정해야함!!!!!!!!!!
+    private fun setTown(
+        latitude: Double,
+        longitude: Double,
+    ) {
+        val geocoder = Geocoder(applicationContext, Locale.KOREAN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocation(
+                latitude,
+                longitude,
+                1,
+            ) { address ->
+                if (address.size != 0) {
+                    Log.d("test", "geocoder ${address[0].thoroughfare}")
+//                    binding.tvMainTown.text = address[0].getAddressLine(0).toString()
+                }
+            }
+        } else {
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+            if (addresses != null) {
+                Log.d("test", "geocoder ${addresses[0].thoroughfare}")
+//                binding.tvMainTown.text = addresses[0].getAddressLine(0).toString()
+            }
         }
     }
 
