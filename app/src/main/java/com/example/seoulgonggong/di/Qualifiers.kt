e11e129c54f5e892ee013cef42aef597e59aa056
hyunji1203 <hj27720@gmail.com>
2024-04-10 21:58:32 +0900

    feat: 레트로핏을 구분해주는 Qualifiers 파일 구분

---
+++ app/src/main/java/com/example/seoulgonggong/di/Qualifiers.kt
@@ -0,0 +1,11 @@
+package com.example.seoulgonggong.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class SeoulOpenApiRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WeatherRetrofit
