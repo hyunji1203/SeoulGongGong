d3e5e357d637ce9c91c9e08fbc2785b664743225
hyunji1203 <hj27720@gmail.com>
2024-04-12 06:31:44 +0900

    feat: 불필요한 파일 삭제

--- app/src/main/java/com/example/seoulgonggong/domain/model/WeatherCategory.kt
+++ app/src/main/java/com/example/seoulgonggong/domain/model/WeatherCategory.kt
@@ -4,11 +4,4 @@ enum class WeatherCategory {
     PTY,
     SKY,
     TMP,
-    OTHER, ;
-
-    companion object {
-        fun findByName(category: String): WeatherCategory {
-            return values().find { it.name == category } ?: OTHER
-        }
-    }
 }
