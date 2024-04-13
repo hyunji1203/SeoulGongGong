dd7b97311220ed433ff4cf369e6985917abe8c52
pingu244 <pingu244@naver.com>
2024-04-14 05:22:37 +0900

    [feat] detail 화면

---
+++ app/src/main/java/com/example/seoulgonggong/ui/facility/detail/SportsFacilityDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.seoulgonggong.ui.facility.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seoulgonggong.R
+
+class SportsFacilityDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sports_facility_detail)
+    }
+}
