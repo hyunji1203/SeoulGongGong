8bb3ab4a5aed831dc2ad2abb498a5c9dcd72c74b
hyunji1203 <hj27720@gmail.com>
2024-04-14 19:43:06 +0900

    refactor: 상수 처리

--- app/src/main/java/com/example/seoulgonggong/ui/main/GeoPointConverter.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/main/GeoPointConverter.kt
@@ -7,20 +7,12 @@ class GeoPointConverter {
         lat: Double,
         lon: Double,
     ): Point {
-        val RE = 6371.00877 // 지구 반경(km)
-        val GRID = 5.0 // 격자 간격(km)
-        val SLAT1 = 30.0 // 투영 위도1(degree)
-        val SLAT2 = 60.0 // 투영 위도2(degree)
-        val OLON = 126.0 // 기준점 경도(degree)
-        val OLAT = 38.0 // 기준점 위도(degree)
-        val XO = 43 // 기준점 X좌표(GRID)
-        val YO = 136 // 기준점 Y좌표(GRID)
-        val DEGRAD = Math.PI / 180.0
+        val degrad = Math.PI / DEGREE
         val re = RE / GRID
-        val slat1 = SLAT1 * DEGRAD
-        val slat2 = SLAT2 * DEGRAD
-        val olon = OLON * DEGRAD
-        val olat = OLAT * DEGRAD
+        val slat1 = SLAT1 * degrad
+        val slat2 = SLAT2 * degrad
+        val olon = OLON * degrad
+        val olat = OLAT * degrad
 
         var sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5)
         sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn)
@@ -29,9 +21,9 @@ class GeoPointConverter {
         var ro = Math.tan(Math.PI * 0.25 + olat * 0.5)
         ro = re * sf / Math.pow(ro, sn)
 
-        var ra = Math.tan(Math.PI * 0.25 + (lat) * DEGRAD * 0.5)
+        var ra = Math.tan(Math.PI * 0.25 + (lat) * degrad * 0.5)
         ra = re * sf / Math.pow(ra, sn)
-        var theta = lon * DEGRAD - olon
+        var theta = lon * degrad - olon
         if (theta > Math.PI) theta -= 2.0 * Math.PI
         if (theta < -Math.PI) theta += 2.0 * Math.PI
         theta *= sn
@@ -40,5 +32,17 @@ class GeoPointConverter {
         val y = (ro - ra * Math.cos(theta) + YO + 0.5).toInt()
 
         return Point(x, y)
+    }
+
+    companion object {
+        private const val RE = 6371.00877 // 지구 반경(km)
+        private const val GRID = 5.0 // 격자 간격(km)
+        private const val SLAT1 = 30.0 // 투영 위도1(degree)
+        private const val SLAT2 = 60.0 // 투영 위도2(degree)
+        private const val OLON = 126.0 // 기준점 경도(degree)
+        private const val OLAT = 38.0 // 기준점 위도(degree)
+        private const val XO = 43 // 기준점 X좌표(GRID)
+        private const val YO = 136 // 기준점 Y좌표(GRID)
+        private const val DEGREE = 180.0
     }
 }
