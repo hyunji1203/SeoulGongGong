05b9b2edc2f78c95919dd677852740abba07fd12
Hyegyeong <gju06061@naver.com>
2024-04-17 06:05:43 +0900

    [feat] 공공 서비스 dataBinding 연결

--- app/src/main/java/com/example/seoulgonggong/ui/feature/public_service_list/recyclerview/PublicServiceViewHolder.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/feature/public_service_list/recyclerview/PublicServiceViewHolder.kt
@@ -1,21 +1,29 @@
 package com.example.seoulgonggong.ui.feature.public_service_list.recyclerview
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.seoulgonggong.databinding.PublicServiceItemBinding
 import com.example.seoulgonggong.ui.model.UiPublicService
 
 class PublicServiceViewHolder private constructor(private val binding: PublicServiceItemBinding) :
     ViewHolder(binding.root) {
     fun bind(data: UiPublicService) {
-        binding.tvPublicServiceItemTitle.text = data.title
-        binding.tvPublicServiceItemPlace.text = data.place
-        binding.tvPublicServiceItemOperatingTime.text = data.operatingTime
-        Glide.with(binding.root)
-            .load(data.img)
-            .into(binding.ivPublicServiceItemImg)
+        binding.uiPublicService = data
+        Glide.with(binding.root).load(data.img).into(object : CustomTarget<Drawable>() {
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                binding.ivPublicServiceItemImg.setImageDrawable(resource)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     companion object {
