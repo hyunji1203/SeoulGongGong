3455d71a3f4e212849e7736a55e5cb5e47dd3cb1
Hyegyeong <gju06061@naver.com>
2024-04-17 06:33:13 +0900

    [feat] 공공 서비스 검색 기능 구현

--- app/src/main/java/com/example/seoulgonggong/domain/repository/PublicServiceRepository.kt
+++ app/src/main/java/com/example/seoulgonggong/domain/repository/PublicServiceRepository.kt
@@ -3,5 +3,5 @@ package com.example.seoulgonggong.domain.repository
 import com.example.seoulgonggong.domain.model.PublicServices
 
 interface PublicServiceRepository{
-    suspend fun getData():Result<PublicServices>
+    suspend fun getServices():Result<PublicServices>
 }
--- app/src/main/java/com/example/seoulgonggong/ui/feature/public_service_list/activity/PublicServiceListActivity.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/feature/public_service_list/activity/PublicServiceListActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
 import com.example.seoulgonggong.databinding.ActivityPublicServiceListBinding
 import com.example.seoulgonggong.ui.feature.public_service_list.recyclerview.PublicServiceAdapter
 import com.example.seoulgonggong.ui.feature.public_service_list.viewmodel.PublicServiceListViewModel
@@ -12,12 +13,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PublicServiceListActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityPublicServiceListBinding
-    val viewModel: PublicServiceListViewModel by viewModels()
+    private lateinit var binding: ActivityPublicServiceListBinding
+    private val viewModel: PublicServiceListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBinding()
+        observeUiState()
+        setUpView()
+        viewModel.getServices()
+    }
+
+    private fun initBinding() {
         binding = ActivityPublicServiceListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun observeUiState() {
         viewModel.uiState.observe(this) {
             if (it.isSuccess == true) {
                 binding.rvPublicServiceList.apply {
@@ -27,11 +39,11 @@ class PublicServiceListActivity : AppCompatActivity() {
                 Toast.makeText(this, it.errorMessage, Toast.LENGTH_SHORT).show()
             }
         }
-        setContentView(binding.root)
-        viewModel.getData()
     }
 
-    private fun initBinding() {
-
+    private fun setUpView() {
+        binding.etSearchServiceList.doOnTextChanged { text, _, _, _ ->
+            viewModel.searchData(text.toString())
+        }
     }
 }
