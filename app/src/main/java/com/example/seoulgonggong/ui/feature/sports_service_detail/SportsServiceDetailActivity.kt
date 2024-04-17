b4dbc0df3efb45f062344ffd2f1cb0032b29b1d6
Hyegyeong <gju06061@naver.com>
2024-04-17 21:18:51 +0900

    [feat] SportsService 링크, 전화번호, 접수일시 BindingAdapter 구현

--- app/src/main/java/com/example/seoulgonggong/ui/feature/sports_service_detail/SportsServiceDetailActivity.kt
+++ app/src/main/java/com/example/seoulgonggong/ui/feature/sports_service_detail/SportsServiceDetailActivity.kt
@@ -2,15 +2,25 @@ package com.example.seoulgonggong.ui.feature.sports_service_detail
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.seoulgonggong.R
+import com.example.seoulgonggong.databinding.ActivitySportsServiceDetailBinding
 import com.example.seoulgonggong.ui.model.UiSportsService
 
 class SportsServiceDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySportsServiceDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sports_service_detail)
+        binding = ActivitySportsServiceDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sportsService = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra(EXTRA_KEY_SPORTS_SERVICE, UiSportsService::class.java) as UiSportsService
+        } else {
+            intent.getSerializableExtra(EXTRA_KEY_SPORTS_SERVICE) as UiSportsService
+        }
+        binding.sportsService = sportsService
     }
 
     companion object {
