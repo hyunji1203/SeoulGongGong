5220e223b4cf5ec292bbb95f2b320bd1b02485e8
pingu244 <pingu244@naver.com>
2024-04-21 02:17:21 +0900

    [feat] widget 기본 구성 요소 추가

---
+++ app/src/main/java/com/seoulfitu/android/ui/AppWidgetProvider.kt
@@ -0,0 +1,44 @@
+package com.seoulfitu.android.ui
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.seoulfitu.android.R
+import com.seoulfitu.android.ui.facility.SportsFacilityActivity
+
+class AppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each widget that belongs to this
+        // provider.
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                    /* context = */ context,
+                    /* requestCode = */  0,
+                    /* intent = */ Intent(context, SportsFacilityActivity::class.java),
+                    /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an onClick listener to
+            // the button.
+            val views: RemoteViews = RemoteViews(
+                    context.packageName,
+                    R.layout.layout_appwidget
+            ).apply {
+                setOnClickPendingIntent(R.id.btn_test, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
