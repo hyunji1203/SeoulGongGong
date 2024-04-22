1fd4e65a302d8940e44a8fb3cb9e7a8e55f11f2a
hyunji1203 <hj27720@gmail.com>
2024-04-23 01:39:55 +0900

    feat: 공공시설 데이터베이스 주입 함수 구현

---
+++ app/src/main/java/com/seoulfitu/android/di/DatabaseModule.kt
@@ -0,0 +1,26 @@
+package com.seoulfitu.android.di
+
+import android.content.Context
+import androidx.room.Room
+import com.seoulfitu.android.data.local.database.SportFacilityScrapDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun providesFacilityScrapDatabase(
+        @ApplicationContext context: Context,
+    ): SportFacilityScrapDatabase =
+        Room.databaseBuilder(
+            context,
+            SportFacilityScrapDatabase::class.java,
+            "SportsFacilityScrap.db",
+        ).build()
+}
