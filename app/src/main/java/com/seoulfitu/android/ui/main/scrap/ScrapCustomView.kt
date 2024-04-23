3d7ee7a6ca6be620370dbe5c45857f184f68bde8
hyunji1203 <hj27720@gmail.com>
2024-04-23 17:00:08 +0900

    feat: 스크랩 커스텀 뷰 구현

---
+++ app/src/main/java/com/seoulfitu/android/ui/main/scrap/ScrapCustomView.kt
@@ -0,0 +1,37 @@
+package com.seoulfitu.android.ui.main.scrap
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.ListAdapter
+import com.seoulfitu.android.databinding.CustomScrapBinding
+import com.seoulfitu.android.databinding.CustomScrapEmptyBinding
+
+class ScrapCustomView(context: Context, attr: AttributeSet? = null) :
+    ConstraintLayout(context, attr) {
+    private lateinit var scrapBinding: CustomScrapBinding
+    private lateinit var emptyBinding: CustomScrapEmptyBinding
+
+    private fun initScrap() {
+        val inflater = LayoutInflater.from(context)
+        scrapBinding = CustomScrapBinding.inflate(inflater, this, true)
+    }
+
+    private fun initEmpty() {
+        val inflater = LayoutInflater.from(context)
+        emptyBinding = CustomScrapEmptyBinding.inflate(inflater, this, true)
+    }
+
+    fun setAdapter(
+        isEmpty: Boolean,
+        adapter: ListAdapter<*, *>,
+    ) {
+        if (isEmpty) {
+            initEmpty()
+        } else {
+            initScrap()
+            scrapBinding.rvScrapList.adapter = adapter
+        }
+    }
+}
