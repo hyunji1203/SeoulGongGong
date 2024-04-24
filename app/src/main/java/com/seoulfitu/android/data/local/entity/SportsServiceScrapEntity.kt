b5a90e589468a02fad051951a960e3fd8bbd2f9b
hyunji1203 <hj27720@gmail.com>
2024-04-24 17:12:59 +0900

    feat: 공공 체육 서비스 로컬 데이터베이스 생성

---
+++ app/src/main/java/com/seoulfitu/android/data/local/entity/SportsServiceScrapEntity.kt
@@ -0,0 +1,33 @@
+package com.seoulfitu.android.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SportsServiceScrap")
+data class SportsServiceScrapEntity(
+    val division: String,
+    val serviceId: String,
+    val mainCategory: String,
+    val subCategory: String,
+    val serviceStatus: String,
+    @PrimaryKey
+    val serviceName: String,
+    val payment: String,
+    val place: String,
+    val user: String,
+    val url: String,
+    val xCoordinate: Double,
+    val yCoordinate: Double,
+    val serviceStartDate: String,
+    val serviceEndDate: String,
+    val registrationStartDate: String,
+    val registrationEndDate: String,
+    val area: String,
+    val imgUrl: String,
+    val details: String,
+    val phoneNumber: String,
+    val operatingStartTime: String,
+    val operatingEndTime: String,
+    val cancellationCriteria: String,
+    val timeLeftForCancellation: Int,
+)
