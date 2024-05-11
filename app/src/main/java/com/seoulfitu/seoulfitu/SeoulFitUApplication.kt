609c8b6fbb58b3edfc11b555585e88b214736240
hyunji1203 <hj27720@gmail.com>
2024-05-11 20:13:12 +0900

    refactor: Application 이름 변경

--- app/src/main/java/com/seoulfitu/seoulfitu/SeoulFitUApplication.kt
+++ app/src/main/java/com/seoulfitu/seoulfitu/SeoulFitUApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SeoulGongGongApplication : Application()
+class SeoulFitUApplication : Application()
